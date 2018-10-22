49bfe8325e4693d5cb88924673362e20762f216f
FliegendeWurst <2012gdwu@web.de>
2018-10-22 22:13:56 +0200

    Show version information on the about screen

--- app/src/main/java/ru/henridellal/emerald/AboutActivity.java
+++ app/src/main/java/ru/henridellal/emerald/AboutActivity.java
@@ -2,11 +2,13 @@ package ru.henridellal.emerald;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity{
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		((TextView) findViewById(R.id.about)).setText(getString(R.string.about, BuildConfig.VERSION_NAME));
 	}
 }
