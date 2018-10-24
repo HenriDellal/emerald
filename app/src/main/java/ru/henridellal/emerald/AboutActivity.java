a99d9c0e42d41920955e7800b31c9027872ce47a
FliegendeWurst <2012gdwu@web.de>
2018-10-24 12:21:42 +0200

    A bugfix and some minor things (#93)
    * Allow normal and dev builds to coexist on the device
    
    * Fix that history would be truncated after launching an app from history
    
    * Show version information on the about screen

--- app/src/main/java/ru/henridellal/emerald/AboutActivity.java
+++ app/src/main/java/ru/henridellal/emerald/AboutActivity.java
@@ -2,11 +2,14 @@ package ru.henridellal.emerald;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity{
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		String versionName = getResources().getString(R.string.version_name);
+		((TextView) findViewById(R.id.about)).setText(getString(R.string.about, versionName));
 	}
 }
