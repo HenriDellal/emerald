21ac723c5601a9aa928d34d291eb6060319c4ce6
HenriDellal <anridellal@yandex.ru>
2018-10-24 13:13:19 +0300

    Update AboutActivity.java

--- app/src/main/java/ru/henridellal/emerald/AboutActivity.java
+++ app/src/main/java/ru/henridellal/emerald/AboutActivity.java
@@ -9,6 +9,7 @@ public class AboutActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-		((TextView) findViewById(R.id.about)).setText(getString(R.string.about, BuildConfig.VERSION_NAME));
+		String versionName = getResources().getString(R.string.version_name);
+		((TextView) findViewById(R.id.about)).setText(getString(R.string.about, versionName));
 	}
 }
